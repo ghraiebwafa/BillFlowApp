da4df52211bbaa0c46fa8a78c8f8ca3139b23520
Wafa Ghraieb <wafaaghraieb@gmail.com>
2026-06-25 17:08:44 +0100

    🗄️ feat(database): add user token version migration

---
+++ Backend/DataAccess/BillFlow.Database/Migrations/20260625153253_UserTokenVersion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillFlow.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserTokenVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TokenVersion",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokenVersion",
+                table: "Users");
+        }
+    }
+}
