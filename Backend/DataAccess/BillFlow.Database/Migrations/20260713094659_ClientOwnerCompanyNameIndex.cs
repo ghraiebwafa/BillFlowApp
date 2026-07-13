63c005ee64c52b8b728a00f92f6820bdcd47cdaa
Wafa Ghraieb <wafaaghraieb@gmail.com>
2026-07-13 10:50:21 +0100

    feat(db): add ClientOwnerCompanyName index

---
+++ Backend/DataAccess/BillFlow.Database/Migrations/20260713094659_ClientOwnerCompanyNameIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BillFlow.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientOwnerCompanyNameIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_OwnerId_CompanyName",
+                table: "Clients",
+                columns: new[] { "OwnerId", "CompanyName" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_OwnerId_CompanyName",
+                table: "Clients");
+        }
+    }
+}
